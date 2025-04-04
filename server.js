e72f299909ddd5810c4c40e4045b7b054e0c53cb
wardhanrharsha <harshaw629@gmail.com>
2025-04-04 16:23:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/api/config/paypal", (req, res) =>
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
   app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
